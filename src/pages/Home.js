80710cdf8795bdc0453b7b37a878e71784e0a680
Xinli WU <ryan7gm@gmail.com>
2022-08-26 04:08:54 +1000

    css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
-import { Stack } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import SearchBox from 'components/SearchBox';
 import React from 'react';
 
 export default function Home() {
   return (
-    <Stack direction='column'>
+    <Stack direction='column' sx={{ height: '100%' }}>
       <div style={{ height: 4 }}></div>
-      <SearchBox />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Box sx={{ top: '20%', position: 'absolute', width: '100%', maxWidth: 768 }}>
+          <SearchBox />
+        </Box>
+      </div>
+
     </Stack>
   );
 }
