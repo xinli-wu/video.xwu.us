4c5ad8623f817a4a9c03b021243f0dba5e5adf5d
Xinli WU <ryan7gm@gmail.com>
2022-08-29 00:58:03 +1000

    <VideoCard />

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,6 +1,7 @@
 import { Box, LinearProgress, Paper, Typography } from '@mui/material';
 import axios from 'axios';
 import React from 'react';
+import { createSearchParams, matchPath, useLocation, useNavigate } from 'react-router-dom';
 import './Video.css';
 import { decode } from 'html-entities';
 
@@ -8,7 +9,9 @@ window.HELP_IMPROVE_VIDEOJS = false;
 
 const SERVER_URL = `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/yt`;
 
-export const NativeVideo = ({ v, title, poster }) => {
+export const VideoCard = ({ v, title, poster }) => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
   const [format, setFormat] = React.useState({ data: undefined, loading: false });
 
   React.useEffect(() => {
@@ -19,23 +22,25 @@ export const NativeVideo = ({ v, title, poster }) => {
     })();
   }, [v]);
 
+  const onTitleClick = (e) => {
+    e.preventDefault();
+
+    if (!matchPath(pathname, '/watch')?.pathname) {
+      navigate({ pathname: '/watch', search: `?${createSearchParams({ v })}` });
+    }
+  };
+
   return (
     <Box>
       <Box sx={{ height: 4 }}>
         {format.loading && <LinearProgress />}
       </Box>
-      <Paper sx={{ alignItems: 'center', minHeight: 220 }}>
-        {format.data && (
-          <video style={{ width: '100%', borderRadius: '4px 4px 0px 0px' }} controls poster={poster} preload="none" controlsList="nodownload">
-            <source src={`${SERVER_URL}/watch?v=${v}`} type={format.data.mimeType} />
-            Your browser does not support HTML video.
-          </video>
-        )}
-        <Box sx={{ cursor: 'pointer', p: 1 }}>
+      <Paper onClick={onTitleClick} sx={{ alignItems: 'center', minHeight: 220, cursor: 'pointer' }}>
+        {format.data && <img alt='' style={{ width: '100%', borderRadius: '4px 4px 0px 0px' }} src={poster} />}
+        <Box sx={{ p: 1 }}>
           {title && <Typography variant='subtitle2' textAlign="left" >{decode(title)}</Typography>}
         </Box>
       </Paper>
     </Box>
-
   );
 };
