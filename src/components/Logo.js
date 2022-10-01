75ff38a0ceefd7e876044b881bd4489be136175c
Xinli WU <ryan7gm@gmail.com>
2022-10-02 01:00:24 +1000

    added logo

---
+++ src/components/Logo.js
@@ -0,0 +1,46 @@
+import { useTheme } from '@mui/material';
+import React from 'react';
+import LogoHubDarkH from '../assets/images/logo/logoHubDarkH.png';
+import LogoHubLightH from '../assets/images/logo/logoHubLightH.png';
+import LogoTubeDarkH from '../assets/images/logo/logoTubeDarkH.png';
+import LogoTubeLightH from '../assets/images/logo/logoTubeLightH.png';
+import LogoHubDarkV from '../assets/images/logo/logoHubDarkV.png';
+import LogoHubLightV from '../assets/images/logo/logoHubLightV.png';
+import LogoTubeDarkV from '../assets/images/logo/logoTubeDarkV.png';
+import LogoTubeLightV from '../assets/images/logo/logoTubeLightV.png';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+function getFaviconEl() {
+  return document.getElementById("favicon");
+}
+
+
+export default function Logo() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const altLogo = Math.floor(Math.random() * 2);
+  const theme = useTheme();
+  const mode = theme.palette.mode;
+
+  const logoH = {
+    'dark': [LogoHubDarkH, LogoTubeDarkH],
+    'light': [LogoHubLightH, LogoTubeLightH]
+  };
+
+  const logoV = {
+    'dark': [LogoHubDarkV, LogoTubeDarkV],
+    'light': [LogoHubLightV, LogoTubeLightV]
+  };
+
+  const favicon = getFaviconEl();
+  if (favicon) favicon.href = logoV[mode][altLogo];
+
+  const onLogoClick = (e) => {
+    if (location.pathname !== '/') navigate({ pathname: '/' });
+  };
+
+  return (
+    <img src={logoH[mode][altLogo]} alt='Logo' onClick={onLogoClick} style={{ cursor: 'pointer' }} />
+  );
+}
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,23 +1,24 @@
-import { Box } from "@mui/material";
-import React from "react";
-import BackBtn from "./BackBtn";
-import ColorModeSwitch from "./ColorModeSwitch";
+import { Box } from '@mui/material';
+import React from 'react';
+import BackBtn from './BackBtn';
+import ColorModeSwitch from './ColorModeSwitch';
+import Logo from './Logo';
 
-function TopBar() {
+export default function TopBar() {
   return (
     <Box
       sx={{
         display: 'flex',
         width: '100%',
+        height: 40,
         justifyContent: 'space-between',
         bgcolor: 'background.default',
         color: 'text.primary'
       }}
     >
       <BackBtn />
+      <Logo />
       <ColorModeSwitch />
     </Box>
   );
 }
-
-export default TopBar;
